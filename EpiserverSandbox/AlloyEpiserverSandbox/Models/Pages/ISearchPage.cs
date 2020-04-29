aa65b7d97cb83fa8c91725a87e04b774d081925f
rmwatson5 <rmwatson5@hotmail.com>
2020-04-29 13:54:51 -0400

    added stuff

---
+++ EpiserverSandbox/AlloyEpiserverSandbox/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace AlloyEpiserverSandbox.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
